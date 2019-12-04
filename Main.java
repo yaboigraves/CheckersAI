1af9ce211d7e506bb96ed6fd09c1a25936312776
unknown <joshchen984@gmail.com>
2019-12-03 16:38:30 -0800

    Josh is cool

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("This will be a kickass checkers game one day!");
+        System.out..println("Josh is cool");
     }
 }
