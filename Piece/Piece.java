f8b88fe256f46935dfd16a41e620e571b3670510
QuinnSimmons05 <qwgs2005@icloud.com>
2019-12-17 17:01:02 -0800

    Character for different color pieces

--- Piece/Piece.java
+++ Piece/Piece.java
@@ -3,13 +3,34 @@ package Piece;
 public class Piece{
     String color;
     Boolean isKing;
+    char pieceChar;
+
     public Piece(String colorArg){
 
-         color = colorArg;
-         isKing = false;
-
-
+        color = colorArg;
+        isKing = false;
+        if (color == "white"){
+            pieceChar = 'w';
+            if (isKing == true){
+                 pieceChar = 'W';
+            }
+        }
+        if (color != "white"){
+            pieceChar = 'b';
+            if (isKing == true){
+                pieceChar = 'B';
+            }
+        } 
+        
     }
+
+         
+
+    public char getPieceChar(){
+        return pieceChar;
+    }
+
+    
     public String  getColor(){
         return color;
     }
